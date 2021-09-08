20211ee092690c7f422d3fd5b410111eb585d350
yesseli <yesselimateo@gmail.com>
2021-09-07 23:21:35 -0600

    Agregar archivos de proyecto.

---
+++ VersionesSO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VersionesSO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int n1 = 5;
+            int n2 = 10;
+            int suma = n1 + n2;
+
+
+
+            Console.WriteLine("el resultado es: " + suma);
+        }
+    }
+}
